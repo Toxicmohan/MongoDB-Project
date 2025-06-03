f5f5f959ac0506eed818ba359dd4f1049feef1d6
Toxicmohan <mk3779056@gmail.com>
2025-06-03 15:35:59 +0530

    Update intermediate.mongodb.rb

--- MongoDB project/dataset/intermediate.mongodb.rb
+++ MongoDB project/dataset/intermediate.mongodb.rb
@@ -1,13 +1,22 @@
 Dataset
+{
 "_id" : 1,
-"name" : "Prithvi",
-favorites: [ "chocolate", "cake", "butter", "apples" ] ,
- { "_id" : 2,
-"name" : "Shivakumar",
-favorites: [ "apples", "pudding", "pie" ]
- { "_id" : 3, "name" :
-"Pradeep",
-favorites: [ "pears", "pecans", "chocolate", "cherries" ]
- { "_id" : 4,
-"name" : "Mukesh",
-favorites: [ "ice cream" ] 
+"details" : [{"name": "Bhumika", "age" : 23} ],
+"marks": [80, 89, 90]
+},
+{
+"_id" : 2, 
+"details" : [{ "name": "Geetha", "age" : 21 ],
+"marks": [90, 85, 70]
+},
+{
+"_id" : 3, 
+"details" : [ { "name": "Mahadevawmay", "age" : 25 } ],
+"marks": [86, 79, 70]
+},
+{
+"_id" : 4, 
+"details" : [{ "name": "Manjunath", "age" : 22}], 
+"marks": [88, 81, 78]
+}
+
